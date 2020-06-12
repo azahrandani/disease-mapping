ea2007ebc68c42598de1b5de2f3e83a6a5860f27
Azahra Putri <azahra.putri@ui.ac.id>
2020-06-12 11:37:31 +0700

    Updates

---
+++ precache-manifest.ac16ae5ee537275c6ae0ad16bdfc0d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5561ab13c3f19f1c34d77569a1f28121",
+    "url": "/diseasemapping/index.html"
+  },
+  {
+    "revision": "d0e822dfa960b036d1de",
+    "url": "/diseasemapping/static/css/main.da26eb76.chunk.css"
+  },
+  {
+    "revision": "fc80c9bf7bc2c0125cd5",
+    "url": "/diseasemapping/static/js/2.60a48946.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diseasemapping/static/js/2.60a48946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e822dfa960b036d1de",
+    "url": "/diseasemapping/static/js/main.82e43eeb.chunk.js"
+  },
+  {
+    "revision": "1eca764a9e4c13caee39",
+    "url": "/diseasemapping/static/js/runtime-main.cc4999a5.js"
+  }
+]);
